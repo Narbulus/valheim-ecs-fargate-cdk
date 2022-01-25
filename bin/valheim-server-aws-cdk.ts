cd9e5f4c4b6a09acd7dfb9dcc60a0cf824cad46b
Narbulus <s.hughes.anderson@gmail.com>
2022-01-24 17:53:37 -0800

    Discord webhooks integration

--- bin/valheim-server-aws-cdk.ts
+++ bin/valheim-server-aws-cdk.ts
@@ -7,4 +7,7 @@ const app = new cdk.App();
 new ValheimServerAwsCdkStack(app, "ValheimServerAwsCdkStack", {
   useFTP: false,
   useServerStatusAPI: true,
+  useDiscordIntegration: true,
+  discordWebhookURL:
+    "https://discord.com/api/webhooks/935332416443252766/UGjkZWNBX7uFFauDGbHTDx3Jid_E0B2FNg-1HaDh91ysTHE82j4afzKOf4tvWgXkXmha",
 });
--- lib/valheim-server-aws-cdk-stack.ts
+++ lib/valheim-server-aws-cdk-stack.ts
@@ -7,6 +7,7 @@ import * as efs from "aws-cdk-lib/aws-efs";
 import { PasswordAuthenticatedFtp } from "./ftp/password-authenticated-ftp";
 import { FtpUser } from "./ftp/ftp-user";
 import { ServerApi } from "./api/server-api";
+import { applyWebooks } from "./discord/apply-webhooks";
 
 export interface ValheimServerProps extends cdk.StackProps {
   /**
@@ -15,6 +16,9 @@ export interface ValheimServerProps extends cdk.StackProps {
   readonly useFTP?: boolean;
 
   readonly useServerStatusAPI?: boolean;
+
+  readonly useDiscordIntegration?: boolean;
+  readonly discordWebhookURL?: string;
 }
 
 export class ValheimServerAwsCdkStack extends cdk.Stack {
@@ -134,8 +138,10 @@ export class ValheimServerAwsCdkStack extends cdk.Stack {
         protocol: ecs.Protocol.UDP,
       }
     );
-
     container.addMountPoints(mountPoint);
+    if (props?.useDiscordIntegration && props.discordWebhookURL) {
+      applyWebooks(container, props.discordWebhookURL);
+    }
 
     const valheimService = new ecs.FargateService(this, "valheimService", {
       cluster: fargateCluster,
@@ -157,10 +163,11 @@ export class ValheimServerAwsCdkStack extends cdk.Stack {
 
     if (props?.useServerStatusAPI) {
       new ServerApi(this, "ServerApi", {
+        clusterArn: fargateCluster.clusterArn,
+        containerDefinition: container,
+        password: "doodle",
         region: this.region,
         service: valheimService,
-        clusterArn: fargateCluster.clusterArn,
-        password: "doodle",
       });
     }
 
